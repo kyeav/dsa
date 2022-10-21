47f630c78fe44adadab0f1c7d2ed884732a52979
Kylie <kylieavie@gmail.com>
2022-10-21 19:12:32 +0800

    productExceptSelf completed

--- questions/productExceptSelf/index.js
+++ questions/productExceptSelf/index.js
@@ -14,7 +14,45 @@
  */
 
 const productExceptSelf = (nums) => {
+    const results = []
 
+    results[0] = 1
+
+    for (let i = 1; i < nums.length; ++i) {
+        results[i] = results[i - 1] * nums[i - 1]
+    }
+
+    let rightProduct = 1
+    for (let i = nums.length - 1; i >= 0; --i) {
+        results[i] = results[i] * rightProduct
+        rightProduct *= nums[i]
+    }
+
+    return results
 };
 
 module.exports = productExceptSelf;
+
+// brute force solution
+// const productExceptSelf = (nums) => {
+//     const leftProducts = []
+//     const rightProducts = []
+//     const results = []
+
+//     leftProducts[0] = 1
+//     rightProducts[nums.length -1] = 1
+
+//     for (let i = 1; i < nums.length; ++i) {
+//         leftProducts[i] = leftProducts[i - 1] * nums[i - 1]
+//     }
+
+//     for (let i = nums.length - 2; i >= 0; --i) {
+//         rightProducts[i] = rightProducts[i + 1] * nums[i + 1]
+//     }
+
+//     for (let i = 0; i < nums.length; ++i) {
+//         results[i] = leftProducts[i] * rightProducts[i]
+//     }
+
+//     return results
+// };
