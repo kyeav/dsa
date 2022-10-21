d2f3fe07124396a50703967f321c3c0c7599c9be
Kylie <kylieavie@gmail.com>
2022-10-21 12:24:39 +0800

    threeSum completed

--- questions/threeSum/index.js
+++ questions/threeSum/index.js
@@ -12,8 +12,43 @@
  */
 
 function threeSum(nums) {
-  
+  nums.sort((a, b) => a - b);
+  const results = [];
+
+  for (i = 0; i < nums.length - 2; ++i) {
+    if (nums[i] === nums[i - 1]) {
+      continue;
+    }
+    let l = i + 1;
+    let r = nums.length - 1;
+
+    while (l < r) {
+      const sum = nums[i] + nums[l] + nums[r];
+
+      if (sum > 0) {
+        r--;
+      }
+      if (sum < 0) {
+        l++;
+      }
+      if (sum === 0) {
+        results.push([nums[i], nums[l], nums[r]]);
+        l++
+        while (nums[l] === nums[l - 1] && l < r) {
+            l++
+        }
+      }
+    }
+  }
+
+  return results;
 }
 
 //DO NOT EDIT BELOW THIS LINE
 module.exports = threeSum;
+
+// brute force solution, n^3 time
+
+// sorting and pointer solution, n^2 time
+// edge case 1: if i === i -1 skip the iteration
+// edge case 2: left pointer can't start at the same number
